b6ee9df1f183dfeb479217649d630d832350440e
louis liao <louis.5407@gmail.com>
2023-08-25 12:24:24 +0800

    讀檔-second-commit

--- chat.py
+++ chat.py
@@ -3,9 +3,9 @@ import os
 #讀取檔案
 def read_flie():
 	lines = []
-	with open("input.txt", "r", encoding = "utf-8") as f:
+	with open("input.txt", "r", encoding = "utf-8-sig") as f:
 		for line in f:
-			lines.append(line)
+			lines.append(line.strip())
 	return lines
 
 
