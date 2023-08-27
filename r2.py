c516bcac6bbc7f06089795994bff3cce15d24023
louis liao <louis.5407@gmail.com>
2023-08-27 23:30:53 +0800

    frist-commit

---
+++ r2.py
@@ -0,0 +1,48 @@
+import os
+
+#讀取檔案
+def read_flie(filename):
+	lines = []
+	with open(filename, "r", encoding = "utf-8-sig") as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def convert(lines):
+	person = None
+	allen_word_count = 0
+	allen_sticker_count = 0
+	viki_word_count = 0
+	viki_sticker_count = 0
+	for line in lines:
+		s = line.split(" ")
+		time = s[0]
+		name = s[1]
+		if name == "Allen":
+			if s[2] == "貼圖":
+				allen_sticker_count += 1
+			else:
+				for m in s[2:]:
+					allen_word_count += len(m)
+
+		elif name == "Viki":
+			if s[2] == "貼圖":
+				viki_sticker_count += 1
+			else:
+				for m in s[2:]:
+					viki_word_count += len(m)
+	print("Allen説"+ str(allen_word_count), ",傳了", allen_sticker_count, "個貼圖")
+	print("viki説"+ str(viki_word_count), ",傳了", viki_sticker_count, "個貼圖")
+
+
+def write_file(filename, lines):
+	with open(filename, "w")as f:
+		for line in lines:
+			f.write(line +"\n")
+
+def main():
+	lines = read_flie("LINE-Viki.txt")
+	lines = convert(lines)
+	#write_file("output.txt", lines)
+
+main()
